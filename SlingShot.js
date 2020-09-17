9c527d9e34ade5bb27bb87099cc83bbd6de56c33
L-Pratham77 <109pratham77@gmail.com>
2020-09-17 14:01:48 +0530

    oridin

---
+++ SlingShot.js
@@ -0,0 +1,22 @@
+class SlingShot {
+    constructor(bodyA, bodyB) {
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            length: 5,
+            stiffness: 0.3
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    display() {
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.sling.bodyB.position;
+
+        strokeWeight(4);
+
+        line(
+            pointA.x, pointA.y,
+            pointB.x, pointB.y);
+    }
+}
